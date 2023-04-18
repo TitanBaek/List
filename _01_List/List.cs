ecfda40c0a28ce11f755b7734b07249f5dd0f476
인권 백 <titanbaek91@gmail.com>
2023-04-18 18:19:38 +0900

    feat : Add,Remove,Grow 추가

---
+++ _01_List/List.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class List<T>
+    {
+        private const int DefaultCapacity = 10;
+        private T[] items;
+        private int size;
+
+        public List()
+        {
+            this.items = new T[DefaultCapacity];
+            this.size = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (size < items.Length)
+            {
+                this.items[this.size++] = item;
+            } else
+            {
+                Grow();
+            }
+        }
+
+        public void Remove(T item)
+        {
+
+        }
+
+        public void Grow()
+        {
+            int newCapacity = items.Length * 2;
+            T[] newItems = new T[newCapacity];
+
+            Array.Copy(items,0 newItems, 0,size);
+            items = newItems;
+        }
+    }
+}
