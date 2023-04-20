7b103675ed787971abdfa1af8a13037b0ddf9efb
인권 백 <titanbaek91@gmail.com>
2023-04-20 20:24:03 +0900

    feat : List,LinkedList 정상동작 확인

--- HomeWork_230420/Program.cs
+++ HomeWork_230420/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("LinkedList :");
+            HomeWork_230420.LinkedList<string> myLinked = new HomeWork_230420.LinkedList<string>();
+
+            myLinked.AddLast("Hello");
+            myLinked.AddLast("How");
+            myLinked.AddLast("Are");
+            myLinked.AddLast("You?");
+
+            foreach(string str in myLinked)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("List :");
+
+            HomeWork_230420.List<string> list = new HomeWork_230420.List<string>();
+            list.Add("안녕하세요.");
+            list.Add("뭘봐요.");
+            list.Add("내 맘이야");
+
+            foreach(string str in list)
+            {
+                Console.WriteLine(str);
+            }
         }
     }
 }
