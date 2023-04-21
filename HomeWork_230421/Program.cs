4d54d69d0af0145c826754f7d6b6d2b454b343f8
인권 백 <titanbaek91@gmail.com>
2023-04-21 21:00:38 +0900

    feat : 각 과제 테스트 함수 추가

--- HomeWork_230421/Program.cs
+++ HomeWork_230421/Program.cs
@@ -2,25 +2,27 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void TestingStack()
         {
             // Adapter Stack 
             HomeWork_230421.AdapterStack<int> myStack = new HomeWork_230421.AdapterStack<int>();
 
-            for(int i = 1 ; i <= 10; i++)
+            for (int i = 1; i <= 10; i++)
             {
                 myStack.Push(i);
             }
 
             while (myStack.Count > 0)
             {
-                Console.WriteLine(myStack.Pop()) ;
+                Console.WriteLine(myStack.Pop());
             }
-            Console.WriteLine();
-            Console.WriteLine();
+        }
+
+        static void TestingQueue()
+        {
             // Queue
-            HomeWork_230421.Queue<int> myQueue = new Queue<int> ();
-            for(int i = 1; i <= 10; i++)
+            HomeWork_230421.Queue<int> myQueue = new Queue<int>();
+            for (int i = 1; i <= 10; i++)
             {
                 myQueue.Enqueue(i);
             }
@@ -29,6 +31,29 @@
             {
                 Console.WriteLine(myQueue.Dequeue());
             }
+        }
+
+        static void TestingBracket()
+        {
+
+        }
+
+        static void TestingPlayer()
+        {
+
+        }
+
+        static void TestingCalculator()
+        {
+
+        }
+
+        static void TestingJosephus()
+        {
+
+        }
+        static void Main(string[] args)
+        {
 
         }
     }
