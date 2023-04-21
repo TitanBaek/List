501450bf9e7d2bc4c1759b95cf21119fa850765d
인권 백 <titanbaek91@gmail.com>
2023-04-21 19:36:26 +0900

    feat : 어뎁터스택 구현

--- HomeWork_230421/AdapterStack.cs
+++ HomeWork_230421/AdapterStack.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace HomeWork_230421
 {
-    internal class AdapterStack
+    internal class AdapterStack<T>
     {
+        private List<T> container;
+        
+        public AdapterStack()
+        {
+            container = new List<T>();
+        }
+
+        public void Push(T item)
+        {
+            container.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (container.Count == 0)
+                throw new InvalidOperationException();
+
+            T result = container[container.Count - 1];
+            container.RemoveAt(container.Count - 1);
+            return result;
+
+        }
+        
+        public T Peek()
+        {
+            if (container.Count == 0)
+                throw new InvalidOperationException();
+
+            T result = container[container.Count - 1];
+            return result;
+        }
     }
 }
