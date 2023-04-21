60f439552422392cea56a5b706fadb45a7669adf
인권 백 <titanbaek91@gmail.com>
2023-04-21 16:47:44 +0900

    feat : Queue 클래스 생성

---
+++ _04_Queue/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _04_Queue
+{
+    internal class Program
+    {
+        /******************************************************
+		 * 큐 (Queue)
+		 * 
+		 * 선입선출(FIFO), 후입후출(LILO) 방식의 자료구조
+		 * 입력된 순서대로 처리해야 하는 상황에 이용
+		 ******************************************************/
+
+        static void Test()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 1; i <= 10; i++) { queue.Enqueue(i); }                     // .Enqueue 로 값 넣기
+
+            while (queue.Count > 0)
+            {
+                Console.WriteLine(queue.Dequeue());                                 // .Dequeue 로 값을 확인
+            }
+        }
+        static void Main(string[] args)
+        {
+                Test();
+        }
+    }
+}
