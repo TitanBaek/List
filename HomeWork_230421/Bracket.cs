05f608f2fcf130144ffbb123ad3c2901d51a100e
인권 백 <titanbaek91@gmail.com>
2023-04-21 21:44:44 +0900

    feat : 괄호 검사기 작업 중
    ㅠㅠ... 머리터지기 일보직전

--- HomeWork_230421/Bracket.cs
+++ HomeWork_230421/Bracket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace HomeWork_230421
 {
     internal class Bracket
     {
+        public bool Main()
+        {
+            string str;
+            Console.Write("괄호를 입력하세요 : ");
+            str = Console.ReadLine();
+
+            if (IsRightRracket(str))
+                return true;
+            else
+                return false;
+        }
+
+        public bool IsRightRracket(string str)
+        {
+            return false;
+        }
     }
 }
