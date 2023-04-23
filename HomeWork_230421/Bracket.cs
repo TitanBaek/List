ef7801af6671e54650bebd45e0b66f382813e98f
인권 백 <titanbaek91@gmail.com>
2023-04-24 01:50:21 +0900

    feat : Program 클래스 Bracket 클래스 변경

--- HomeWork_230421/Bracket.cs
+++ HomeWork_230421/Bracket.cs
@@ -9,21 +9,197 @@ namespace HomeWork_230421
 {
     internal class Bracket
     {
-        public bool Main()
+        public void Main()
         {
+
             string str;
+            string temp = "";
+            int leftCount = 0;
+            int rightCount = 1;
+
             Console.Write("괄호를 입력하세요 : ");
             str = Console.ReadLine();
 
-            if (IsRightRracket(str))
-                return true;
-            else
-                return false;
-        }
+            char[] cArray = str.ToCharArray(0, str.Length);
 
-        public bool IsRightRracket(string str)
+            HomeWork_230421.AdapterStack<char> bracketStack = new AdapterStack<char>();
+
+            for (int i = 0; i < cArray.Length; i++)
+            {
+                bracketStack.Push(cArray[i]);
+            }
+
+            // Pop해서 값 하나를 꺼내서 temp 변수에 넣어주고
+            // 반복문 한 번돌고 다음 Pop에서 temp와 Pop한 데이터를 비교해줘서 
+            // 정답인지 아닌지 판단하는 로직을 우선 구현해보자.
+
+            while (bracketStack.Count > 0)
+            {
+                if (temp == "")
+                {
+                    temp = bracketStack.Pop().ToString();
+                    continue;
+                }
+                if (!IsOkay(temp, bracketStack.Pop().ToString(), ref leftCount, ref rightCount))
+                {
+                    Console.WriteLine("일치하지 않아요.");
+                    break;
+                }
+
+                temp = "";
+            }
+
+            if (leftCount == rightCount)
+            {
+                Console.WriteLine("일치합니다.");
+            }
+            Console.WriteLine("끝");
+            Console.WriteLine();
+
+        }
+        public static bool IsOkay(string left, string right, ref int leftCount, ref int rightCount)
         {
-            return false;
+            Console.WriteLine($"Left : {left} Right : {right} ");
+            bool isOkay = false;
+            switch (left)
+            {
+                case "]":
+                    {
+                        if (right == "[")
+                        {
+                            leftCount++;
+                        }
+                        else if (right == "]")
+                        {
+                        }
+                        else
+                        {
+                            isOkay = false;
+                        }
+                        break;
+                    }
+                case ")":
+                    {
+                        if (right == "(")
+                        {
+                            leftCount++;
+                        }
+                        else if (right == ")")
+                        {
+                            rightCount++;
+                        }
+                        else
+                        {
+                            isOkay = false;
+                        }
+                        break;
+                    }
+                case "}":
+                    {
+                        if (right == "{")
+                        {
+                            leftCount++;
+                        }
+                        else if (right == "}")
+                        {
+                            rightCount++;
+                        }
+                        else
+                        {
+                            isOkay = false;
+                        }
+                        break;
+                    }
+                case ">":
+                    {
+                        if (right == "<")
+                        {
+                            leftCount++;
+                        }
+                        else if (right == ">")
+                        {
+                            rightCount++;
+                        }
+                        else
+                        {
+                            isOkay = false;
+                        }
+                        break;
+                    }
+                case "[":
+                    {
+                        if (right == "]")
+                        {
+                            rightCount++;
+                        }
+                        else if (right == "[")
+                        {
+                            leftCount++;
+                        }
+                        else
+                        {
+                            isOkay = false;
+                        }
+                        break;
+                    }
+                case "(":
+                    {
+                        if (right == ")")
+                        {
+                            rightCount++;
+                        }
+                        else if (right == "(")
+                        {
+                            leftCount++;
+                        }
+                        else
+                        {
+                            isOkay = false;
+                        }
+                        break;
+                    }
+                case "{":
+                    {
+                        if (right == "}")
+                        {
+                            rightCount++;
+                        }
+                        else if (right == "{")
+                        {
+                            leftCount++;
+                        }
+                        else
+                        {
+                            isOkay = false;
+                        }
+                        break;
+                    }
+                case "<":
+                    {
+                        if (right == ">")
+                        {
+                            rightCount++;
+                        }
+                        else if (right == "<")
+                        {
+                            leftCount++;
+                        }
+                        else
+                        {
+                            isOkay = false;
+                        }
+                        break;
+                    }
+                default: isOkay = false; break;
+
+            }
+
+            Console.WriteLine($"{leftCount} & {rightCount}");
+            if (leftCount == rightCount && isOkay)
+            {
+                isOkay = leftCount == rightCount ? true : false;
+            }
+            return isOkay;
         }
     }
 }
--- HomeWork_230421/Program.cs
+++ HomeWork_230421/Program.cs
@@ -36,73 +36,8 @@ namespace HomeWork_230421
         }
 
         static void Main(string[] args)
-        {
-            string str;
-            string temp = "";
-            int leftCount = 0;
-            int rightCount = 0;
+        {}
 
-            Console.Write("괄호를 입력하세요 : ");
-            str = Console.ReadLine();
-
-            char[] cArray = str.ToCharArray(0, str.Length);
-
-            HomeWork_230421.AdapterStack<char> bracketStack = new AdapterStack<char>();
-
-            for (int i = 0; i < cArray.Length; i++)
-            {
-                bracketStack.Push(cArray[i]);
-            }
-
-            // Pop해서 값 하나를 꺼내서 temp 변수에 넣어주고
-            // 반복문 한 번돌고 다음 Pop에서 temp와 Pop한 데이터를 비교해줘서 
-            // 정답인지 아닌지 판단하는 로직을 우선 구현해보자.
-
-            while(bracketStack.Count > 0)
-            {
-                if (temp == "")                                 // temp에 값이 없으면 Pop을 넣음
-                {
-                    temp = bracketStack.Pop().ToString();
-                } else if (temp == bracketStack.Peek().ToString()) {    // temp의 값과 Peek의 값이 같다? 다중괄호란뜻..;
-                    rightCount++;
-                } else
-                {
-                    Console.WriteLine($"Temp : {temp}  NowPop : {bracketStack.Peek()} ");
-
-                    if (IsOkay(temp, bracketStack.Pop().ToString()))
-                    {
-                        temp = "";
-                        Console.WriteLine("일치하는 괄호");
-                    }
-                    else
-                    {
-                        Console.WriteLine("일치하지 않는 괄호");
-                        break;
-                    }
-                }           
-
-            }
-
-            Console.WriteLine("끝");
-            Console.WriteLine();
-        }
-
-        public static bool IsOkay(string left,string right)
-        {
-            Console.WriteLine($"Left : { left } Right : {right} ");
-            bool isOkay = false;
-            switch (left)
-            {
-                case "]": { isOkay = right == "[" ? true : false; break; }
-                case ")": { isOkay = right == "(" ? true : false; break; }
-                case "}": { isOkay = right == "{" ? true : false; break; }
-                case ">": { isOkay = right == "<" ? true : false; break; }
-
-                default: isOkay = false; break;
-
-            }
-
-            return isOkay;
-        }
+        
     }
 }
