c8cfdf99bc11fc88277e510cb1ad602693aa0593
인권 백 <titanbaek91@gmail.com>
2023-04-28 02:57:39 +0900

    feat : 아아아악

--- HomeWork_230427/Program.cs
+++ HomeWork_230427/Program.cs
@@ -43,28 +43,100 @@ namespace HomeWork_230427
         }
         //
 
+        /// <summary>
+        /// N&M3 과제..
+        /// </summary>
+        static int n, m;
+        static StringBuilder sb2 = new StringBuilder();
+        static int[] numArray;
         static void NandM()
         {
-            StringBuilder mySb = new StringBuilder();
-            int n = 4;
-            int m = (int)Math.Pow(2,2);
+            string inputNM = Console.ReadLine();
 
-            for (int j = 1; j <= n; j++)
+            string[] nm = inputNM.Split(" ");
+
+            n = Int32.Parse(nm[0]);
+            m = Int32.Parse(nm[1]);
+            numArray = new int[m];
+
+            DoNandM(0);
+            
+;
+        }
+
+        static void DoNandM(int depth)
+        {
+            if(depth == m) // depth가 입력된 M과 같다면
             {
-                for (int i = 1; i <= m; i++)
+                for(int i = 0; i < numArray.Length; i++)
                 {
-                    mySb.Append($"{j} {i} \n");
+                    sb2.Append($"{numArray[i]} ");
                 }
-
+                sb2.Append('\n');
+                return;
             }
 
-            Console.WriteLine(mySb.ToString());
+            for(int i = 0; i < n; i++)
+            {
+                numArray[depth] = i + 1;
+                DoNandM(depth+1);
+            }
+        }
+
+
+        /// <summary>
+        /// 잃어버린 괄호..과제..
+        /// 백준에서만 컴파일(Format Exception) 에러 도대체 왜 나는건지 모르겠습니다...
+        /// </summary>
+        static void Bracket()
+        {
+            // 55-50+40
+            string inputMath = Console.ReadLine();
+
+            string[] plusArray;
+            string[] minusArray;
+            int result = 0;
+            int result2 = 0;
+
+            if (inputMath.Contains("-"))
+            {
+                minusArray = inputMath.Split("-");         // - 를 기준으로 Split
+                for (int i = 0; i < minusArray.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        result = int.Parse(minusArray[i]);
+                        continue;
+                    }
+
+                    if (minusArray[i].Contains("+"))
+                    {
+                        plusArray = minusArray[i].Split("+");
+                        for(int j = 0; j < plusArray.Length; j++)
+                        {
+                            result2 += int.Parse(plusArray[j]);
+                        }
+                    } else
+                    {
+                        result -= int.Parse(minusArray[i]);
+                    }
+                }
+
+                result -= result2;
+            } else
+            {
+                plusArray = inputMath.Split('+');
+                for (int i = 0; i < plusArray.Length; i++)
+                    result += Convert.ToInt32(plusArray[i]);
+            }
+            Console.WriteLine(result);
+
         }
 
 
         static void Main(string[] args)
         {
-            NandM();
+            Bracket();
         }
     }
 }
