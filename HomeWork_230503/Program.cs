98eed370ecc87eec156a0fa5ae9317805889122e
인권 백 <titanbaek91@gmail.com>
2023-05-04 01:54:02 +0900

    feat : 에이스타과제

---
+++ HomeWork_230503/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork_230503
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
