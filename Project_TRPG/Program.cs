1e19213b1030ef5c0ef79d3a0e50bbe9730b3515
인권 백 <baek626@gmail.com>
2023-05-08 15:39:36 +0900

    feat : 텍스트 알피지 만들기 시작

---
+++ Project_TRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_TRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
